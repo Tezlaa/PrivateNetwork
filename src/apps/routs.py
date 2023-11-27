c7e2012b13cabfe4be523229376928b99680403e
Bogdan Tishenko <bogdantihenko60@gmail.com>
2023-11-27 21:22:42 +0200

    added formater suffix

--- src/apps/routs.py
+++ src/apps/routs.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
     path('chat/', include('apps.chat.urls')),
     path('', include('apps.lobby.urls')),
     path('account/', include('apps.account.urls')),
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
