9de7a63cb2dcd688d30f69e689d466a1324befa7
Bogdan Tishenko <bogdantihenko60@gmail.com>
2023-11-27 21:23:03 +0200

    delete suffix

--- src/apps/routs.py
+++ src/apps/routs.py
@@ -1,13 +1,8 @@
 from django.urls import path, include
 
-from rest_framework.urlpatterns import format_suffix_patterns
-
 
 urlpatterns = [
     path('chat/', include('apps.chat.urls')),
     path('', include('apps.lobby.urls')),
     path('account/', include('apps.account.urls')),
 ]
-
-
-urlpatterns = format_suffix_patterns(urlpatterns)
